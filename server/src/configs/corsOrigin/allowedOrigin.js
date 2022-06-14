1dca2ec36a510554df25e157397e614f6bad2ce4
Khaled Al Najjar <knajjar@mykloud.io>
2022-06-14 23:21:42 +0300

    Adding cors

--- server/src/configs/corsOrigin/allowedOrigin.js
+++ server/src/configs/corsOrigin/allowedOrigin.js
@@ -1,8 +1,10 @@
 const allowedOrigins = [
-  'http://127.0.0.1:8080',
-  'http://localhost:8080',
-  'http://127.0.0.1:8765',
-  'http://localhost:8765',
+  "http://127.0.0.1:8080",
+  "http://localhost:8080",
+  "http://127.0.0.1:8765",
+  "http://localhost:8765",
+  "https://mykmail-server-usa.herokuapp.com",
+  "https://mykmail-server-eu.herokuapp.com",
 ];
 
 module.exports = allowedOrigins;
