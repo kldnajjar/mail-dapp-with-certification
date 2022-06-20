6e2b545732cf2ca16e47100b419d725ace44d81f
Khaled Al Najjar <knajjar@mykloud.io>
2022-06-20 12:54:39 +0300

    Tweaking profile

--- client/src/views/Profile/index.js
+++ client/src/views/Profile/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useGunContext from "../../context/useGunContext";
-import useSessionChannel from "../../hooks/useSessionChannel";
 
 import Header from "../Header";
 import Sidebar from "../Sidebar";
@@ -17,7 +16,6 @@ const Profile = () => {
   const [firstName, setFirstName] = useState("");
 
   let navigate = useNavigate();
-  const sessionChannel = useSessionChannel();
 
   const { getGun, getUser, getCertificate, setCertificate } = useGunContext();
   const profile = JSON.parse(sessionStorage.getItem("profile"));
@@ -29,35 +27,6 @@ const Profile = () => {
 
     setFirstName(profile.firstName);
   }, []);
-
-  const pageRedirection = (page) => {
-    navigate(page, { replace: true });
-  };
-
-  const logOut = (evt) => {
-    setCertificate(null);
-
-    const user = getUser();
-
-    user.leave();
-
-    // check if logout failed, if so manually remove
-    // user from session storage
-    // see https://gun.eco/docs/User#user-leave
-    if (user._.sea) {
-      window.sessionStorage.removeItem("pair");
-    }
-
-    // logged out from click, notify other tabs
-    if (evt) {
-      sessionChannel.postMessage({
-        eventName: "REMOVE_YOUR_CREDS",
-      });
-    }
-
-    sessionStorage.removeItem("profile");
-    pageRedirection("/");
-  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -84,14 +53,6 @@ const Profile = () => {
 
   const renderProfile = () => {
     return (
-      // <div>
-      //   <h1>community</h1>
-      //   <div>
-      //     logged in as {getProfile().firstName} ({getProfile().email}).{' '}
-      //     <button onClick={logOut}>Log out</button>
-      //   </div>
-      //   <h2>user profiles</h2>
-      // </div>
       <div className="app">
         <Header />
         <div className="app-body">
